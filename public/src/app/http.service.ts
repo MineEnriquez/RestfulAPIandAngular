c1e74553cb3bf4e375bdf785be790a2fa183c1cd
MineEnriquez <38847090+MineEnriquez@users.noreply.github.com>
2019-11-09 19:28:26 -0800

    Restful Tasks Assignment
    Building off  your Restful Task API assignment, add a service. Add functions to your service that make AJAX requests to all the get routes you made for the Restful Task API. This includes retrieving all tasks and retrieving a task by ID. Depending on how you wrote your API, it may also include deleting a task by ID. For now, have the service print the data it receives to the console. To complete this assignment, the service will need to be created, registered, and injected into the root component. Also, you'll need HttpClient in order to communicate with your server.
     Create a service
     Register the service
     Import HttpClientModule into the project
     Inject HttpClient into the service
     Inject the service into the root component
     Write functions in the service that make AJAX requests to all get routes in the Restful Task API
     Invoke each function you wrote in the service and have the service console.log the data it receives

---
+++ public/src/app/http.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { request } from 'http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private _http: HttpClient) {
+    this.getTasks();
+    this.getById("5dc76f608d5c09540cf7d82f");
+    this.getByTitle("task 4");
+  }
+  getTasks() {
+    // our http response is an Observable, store it in a variable
+    let tempObservable = this._http.get('/api/tasks/retrieveall');
+    // subscribe to the Observable and provide the code we would like to do with our data from the response
+    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+  }
+  getById(_id: string) {
+
+    let tempobservable = this._http.get('/api/tasks/retrieveId/' + _id);
+    tempobservable.subscribe(data => console.log("Got our tasks!", data));
+
+  }
+  getByTitle(_title: string) {
+    let tempobservable = this._http.get('/api/tasks/retrieveTitle/' + _title);
+    tempobservable.subscribe(data => console.log("Got our tasks!", data));
+  }
+
+}
+
